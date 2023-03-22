7e35140724a1c10ae41d4e4ed675d7d1c7761571
ykla <yklaxds@gmail.com>
2023-03-23 03:34:22 +0800

    Update sidebar.ts

--- docs/.vuepress/configs/sidebar.ts
+++ docs/.vuepress/configs/sidebar.ts
@@ -7,10 +7,14 @@ import type {SidebarConfig} from '@vuepress/theme-default';
 export const sideBarConfig: SidebarConfig = [
   {
     text: '个人文档',
-    children: [
-	       '/README.md',
-	       '/ce-shi-8.md',
-	       '/ppp657.md'
-	      ],
+    children: ['/README.md'],
+  },
+    {
+    text: '测试345',
+    children: ['/ce-shi-8.md'],
+  },
+  {
+    text: '测试1234',
+    children: ['/ppp657.md'],
   },
 ];
