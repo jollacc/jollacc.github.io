db23aa50c65a5fd5e4f366d05c7ce06b36beb41b
jollacc <32416419+jollacc@users.noreply.github.com>
2023-03-23 08:01:46 +0800

    Update sidebar.ts

--- docs/.vuepress/configs/sidebar.ts
+++ docs/.vuepress/configs/sidebar.ts
@@ -9,7 +9,7 @@ export const sideBarConfig: SidebarConfig = [
     text: '个人文档',
     children: ['/README.md'],
   },
-    {
+  {
     text: '测试345',children: [
     text: '3344',children: ['/ce-shi-8.md']
     ],
